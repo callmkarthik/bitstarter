360be4a6bf25eb676aea4cae54ee9b0d165782ae
Karthik Murugesan <callmkarthik@gmail.com>
2013-07-15 04:01:29 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
-
+var fs = require('fs');
+var buf = require('buffer');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	buf = new Buffer(fs.readFileSync("index.html"));
+	response.send(buf.toString());
+	//response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
